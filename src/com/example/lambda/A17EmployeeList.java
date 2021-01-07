3081dc1e41694ee446dc1c61d73d8d45b7c8603d
Helmi Hartanto <>
2021-01-07 10:56:15 +0700

    First Commit

---
+++ src/com/example/lambda/A17EmployeeList.java
@@ -0,0 +1,12 @@
+package com.example.lambda;
+
+import java.util.List;
+
+public class A17EmployeeList {
+    public static void main(String[] args) {
+        
+        List<Employee> eList = Employee.createShortList();
+        
+        eList.forEach(Employee::printSummary);
+    }
+}
---
+++ src/com/example/lambda/Main.java
@@ -0,0 +1,10 @@
+package com.example.lambda;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+      A01MapTest.main(args);
+    
+  }
+}
